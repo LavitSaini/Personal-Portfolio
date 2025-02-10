dfe84a9f8e2f2947c4c3870161efe8d70d7d8336
Lavit Saini <115241923+LavitSaini@users.noreply.github.com>
2025-02-10 15:00:08 +0530

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -62,7 +62,7 @@ const Contact = () => {
 
   return (
     <section
-      className="container px-8 pt-16 pb-28 flex flex-col gap-12 items-center custom-sm:py-12"
+      className="container px-8 pt-16 pb-36 flex flex-col gap-12 items-center custom-sm:py-12"
       id="contact"
     >
       <div className="flex flex-col gap-1.5 items-center">
